92dd8146cd7b0e3b100a425ca51bef09a9992029
Umang Banugaria <uxb3@case.edu>
2011-10-23 23:03:41 -0400

    Created empty Android project

---
+++ src/edu/cwru/eecs393/MusicPlayer2Activity.java
@@ -0,0 +1,13 @@
+package edu.cwru.eecs393;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MusicPlayer2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
