6634f909c8950d3173d3cdfb8513f8d78acc427a
Umang Banugaria <uxb3@case.edu>
2011-10-30 17:05:35 -0400

    Added the Splash Screen and Music Selection activities.

--- src/edu/cwru/eecs393/MusicPlayer2Activity.java
+++ src/edu/cwru/eecs393/MusicPlayer2Activity.java
@@ -1,13 +1,47 @@
 package edu.cwru.eecs393;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MusicPlayer2Activity extends Activity {
+	protected static final long SPLASHTIME = 2500;
+	protected boolean active = true;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // removes title bar on app, making image full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.main);
+        
+        // thread for displaying the SplashScreen
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while(active && (waited < SPLASHTIME)) {
+                        sleep(100);
+                        if(active) {
+                            waited += 100;
+                        }
+                    }
+                } catch(InterruptedException e) {
+                    // do nothing
+                } finally {
+                    finish();
+                    startActivity(new Intent(MusicPlayer2Activity.this, Selection.class));
+                    stop();
+                }
+            }
+        };
+        splashTread.start();
     }
 }
---
+++ src/edu/cwru/eecs393/Selection.java
@@ -0,0 +1,16 @@
+package edu.cwru.eecs393;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Selection extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	
+    	
+    }
+	
+}
