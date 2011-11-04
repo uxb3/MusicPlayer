0dab1fd9182e67281185ac8cf0b05bcf9e95c27a
Umang Banugaria <uxb3@case.edu>
2011-11-04 15:30:17 -0400

    new files, mostly just the images for the player activity

---
+++ src/edu/cwru/eecs393/PlayerListener.java
@@ -0,0 +1,46 @@
+package edu.cwru.eecs393;
+
+import java.io.IOException;
+
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+
+public class PlayerListener implements OnCompletionListener {
+
+	public PlayerListener()
+	{
+		
+	}
+	
+	public void onCompletion(MediaPlayer mp) {
+		if(PlayerState.currentSong >= PlayerState.nowPlaying.size())
+		{
+			mp.stop();
+		}
+		else
+		{			
+			try {
+				PlayerState.currentSong++;
+				PlayerState.mp.reset();
+				PlayerState.mp.setDataSource(PlayerState.context, PlayerState.nowPlaying.get(PlayerState.currentSong).getURI());
+				PlayerState.mp.prepare();
+				PlayerState.mp.start();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+			
+		}
+		
+	}
+
+}
