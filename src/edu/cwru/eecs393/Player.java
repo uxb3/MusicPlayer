fe9da082695122e26fc1944cd46a25405ad7b4d6
Umang Banugaria <uxb3@case.edu>
2011-12-15 20:35:26 -0500

    made player text size smaller

--- src/edu/cwru/eecs393/Player.java
+++ src/edu/cwru/eecs393/Player.java
@@ -4,11 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
